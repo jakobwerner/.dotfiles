cb242937c8be3920d40519cc7a7cbc48141e0a7b
Jakob Werner <jdw@posteo.net>
2023-02-18 11:03:58 +0100

    Added lua_ls

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -13,34 +13,6 @@ local check_backspace = function()
     return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
 end
 
-local kind_icons = {
-    Text = "",
-    Method = "",
-    Function = "",
-    Constructor = "",
-    Field = "",
-    Variable = "",
-    Class = "",
-    Interface = "",
-    Module = "",
-    Property = "",
-    Unit = "",
-    Value = "",
-    Enum = "",
-    Keyword = "",
-    Snippet = "",
-    Color = "",
-    File = "",
-    Reference = "",
-    Folder = "",
-    EnumMember = "",
-    Constant = "",
-    Struct = "",
-    Event = "",
-    Operator = "",
-    TypeParameter = "",
-}
-
 cmp.setup {
     snippet = {
         expand = function(args)
@@ -49,8 +21,8 @@ cmp.setup {
     },
 
     mapping = cmp.mapping.preset.insert {
-        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+        ["<C-k>"] = cmp.mapping(cmp.mapping.scroll_docs( -1), { "i", "c" }),
+        ["<C-j>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
         ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
         ["<C-e>"] = cmp.mapping {
             i = cmp.mapping.abort(),
@@ -72,53 +44,48 @@ cmp.setup {
                 fallback()
             end
         end, {
-        "i",
-        "s",
-    }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-            cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-        else
-            fallback()
-        end
-    end, {
-    "i",
-    "s",
-}),
-  },
-  formatting = {
-      fields = { "kind", "abbr", "menu" },
-      format = function(entry, vim_item)
-          vim_item.kind = kind_icons[vim_item.kind]
-          vim_item.menu = ({
-              nvim_lsp = "LSP",
-              nvim_lua = "Lua",
-              luasnip = "Snippet",
-              buffer = "Buffer",
-              path = "Path",
-              emoji = "Emoji",
-          })[entry.source.name]
-          return vim_item
-      end,
-  },
-  sources = {
-      { name = "nvim_lsp" },
-      { name = "nvim_lua" },
-      { name = "luasnip" },
-      { name = "buffer" },
-      { name = "path" },
-  },
-  confirm_opts = {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = false,
-  },
-  window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-  },
-  experimental = {
-      ghost_text = true,
-  },
+            "i",
+            "s",
+        }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable( -1) then
+                luasnip.jump( -1)
+            else
+                fallback()
+            end
+        end, {
+            "i",
+            "s",
+        }),
+    },
+    formatting = {
+        fields = { "abbr", "menu" },
+        format = function(entry, vim_item)
+            vim_item.menu = ({
+                    nvim_lsp = "lsp",
+                    nvim_lua = "lua",
+                    luasnip = "snippet",
+                    buffer = "buffer",
+                    path = "path",
+                })[entry.source.name]
+            return vim_item
+        end,
+    },
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "nvim_lua" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+    },
+    confirm_opts = {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = false,
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
 }
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,7 +1,7 @@
 -- Automatically install packer
 local ensure_packer = function()
     local fn = vim.fn
-    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
     if fn.empty(fn.glob(install_path)) > 0 then
         fn.system({
             'git',
@@ -45,7 +45,7 @@ return require('packer').startup(function(use)
     use "hrsh7th/cmp-buffer" -- buffer completions
     use "hrsh7th/cmp-path" -- path completions
     use "saadparwaiz1/cmp_luasnip" -- snippet completions
-    use "hrsh7th/cmp-nvim-lsp" 
+    use "hrsh7th/cmp-nvim-lsp"
     use "hrsh7th/cmp-nvim-lua"
 
     -- snippets
